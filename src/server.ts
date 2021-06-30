6c36939c4d08bbfe2f2b08bb534ad02763ca00eb
Thiago Carvalho <thgcarvalho@icloud.com>
2021-06-29 21:25:44 -0300

    feature: 01 and 02

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
+import "reflect-metadata";
 import express from "express";
+
+import { router } from "./routes";
+
+import "./database";
 
 const app = express();
 
-app.get('/test', (request, response)=> {
-    return response.send("olá NLW");
-});
+app.use(express.json());
 
-app.listen(3000, () => console.log('Server is running...'));
+app.use(router);
+
+app.listen(3000, () => console.log("Server is running"));
